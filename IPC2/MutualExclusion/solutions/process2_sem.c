d0bd88f8ffd5c2a943c00ca8154a5ec11abe6799
Calvin Kam <calvinkam123@gmail.com>
2017-03-13 12:43:59 +0800

    Separate Questions and Solutions for Mutual Exclusion

--- IPC2/MutualExclusion/solutions/process2_sem.c
+++ IPC2/MutualExclusion/solutions/process2_sem.c
@@ -4,15 +4,22 @@
 #include <string.h>
 #include <fcntl.h>
 #include <sys/stat.h>
+#include <semaphore.h>
 #include <unistd.h>
 int main(int argc, char * argv[]) {
+    sem_t * mutex;
     char * c = "This is CSCI3150--An operating system course.\n";
     // specify no buffering for stderr
     setbuf(stderr, NULL);  
+    mutex = sem_open("mutex_for_stderr", O_CREAT, 0666, 1);
+    sem_wait(mutex);
     while (* c != '\0') {
         fputc(* c, stderr);
         c++;
         sleep(rand()%2+1);
     }
+    sem_post(mutex);
+    sem_close(mutex);
+    sem_unlink("mutex_for_stderr");
     return 0;
 }
